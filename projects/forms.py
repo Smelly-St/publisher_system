bfa299c700878a33826f866df02aa1dbe5fc4800
Smelly-St <stasya10chur@icloud.com>
2025-06-19 01:18:36 +0300

    Update forms.py

--- projects/forms.py
+++ projects/forms.py
@@ -7,7 +7,12 @@ class ProjectForm(forms.ModelForm):
         model = Project
         fields = '__all__'
         widgets = {
-            'start_date': forms.DateInput(attrs={'type': 'date'}),
-            'end_date': forms.DateInput(attrs={'type': 'date'}),
-            'description': forms.Textarea(attrs={'rows': 3}),
+            'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'end_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+            'budget': forms.NumberInput(attrs={'step': 'any', 'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-select'}),
+            'manager': forms.Select(attrs={'class': 'form-select'}),
+            'author': forms.Select(attrs={'class': 'form-select'}),
+            'manuscript': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
